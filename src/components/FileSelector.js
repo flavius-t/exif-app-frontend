bc8fdff26b5fe6ae565dd22af42bfe3f5462a198
flavius-t <flavius.t@hotmail.com>
2023-06-06 22:38:03 -0700

    remove folder upload, restrict accepted file types

--- src/components/FileSelector.js
+++ src/components/FileSelector.js
@@ -3,11 +3,7 @@ import React from 'react';
 function FileSelector({ onFilesSelected, onFolderSelected }) {
     return (
         <div>
-            <label htmlFor="file-input" className="input-label">
-                Select Files
-            </label>
-            <input id="file-input" type="file" multiple onChange={onFilesSelected} />
-            <input id="folder-input" type="file" directory="" webkitdirectory="" onChange={onFolderSelected} /> 
+            <input type="file" accept="image/jpg, image/jpeg, image/png" multiple onChange={onFilesSelected} />
         </div>
     );
 }
