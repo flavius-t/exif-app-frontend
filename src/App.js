d5a4eab2979795d192cc718b99da0eab5b4e4f0e
flavius-t <flavius.t@hotmail.com>
2023-06-09 18:07:46 -0700

    add endpoint routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import FileSelectorContainer from './containers/FileSelectorContainer';
+
 
 function App() {
   return (
     <div className="App">
-        <h1>EXIF Eraser App</h1>
-        <FileSelectorContainer />
+        <h1>EXIF App</h1>
+        <Routes>
+          <Route path="/" element={<FileSelectorContainer />} />
+          <Route path="/upload" element={<FileSelectorContainer />} />
+        </Routes>
     </div>
   );
 }
