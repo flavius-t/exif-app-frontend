6fb28384db6856dbe10711f9af2687c4591eaeff
flavius-t <flavius.t@hotmail.com>
2023-07-07 15:23:14 -0400

    add user info text

--- src/components/FileSelector.js
+++ src/components/FileSelector.js
@@ -36,6 +36,31 @@ function FileSelector({ onDragEnter, onDragLeave, onDrop, onFileInputChange, onU
         <div id="file-upload-container">
             { isProcessing && processing_message}
             { !isProcessing && upload_area}
+            <p>
+                <strong>Supported file types:</strong> {allowedFileTypes.join(', ')}
+            </p>
+            <div id='welcome-text'>
+                <h3>What is EXIF data?</h3>
+                <p>
+                    EXIF data is metadata that is automatically added to image files by digital cameras and smartphones.
+                    This data can include the date and time the photo was taken, the camera settings used, and even the
+                    location where the photo was taken. This data can be useful, if for example you are a photographer, but it can also be a privacy concern.
+                    You may not want to share such information with others.
+                </p>
+                <h3>What does this tool do?</h3>
+                <p>
+                    This tool allows you to upload images and download copies of those images with the EXIF data removed.
+                    It also allows you to view the extracted EXIF data for each image, and download it as well.
+                </p>
+                <h3>Why Remove Exif Data?</h3>
+                <p>
+                    There are many reasons why you might want to remove EXIF data from your images. As previously mentioned
+                    you may be concerned about privacy. However, another benefit of removing EXIF data is that it can significantly reduce
+                    image file size. For example, a 5MB image can be reduced to 1MB or less by removing EXIF data, without affecting image quality.
+                    This may not be in your interest if you wish to preserve the EXIF data, but if you are just sharing images online, or don't plan on using the EXIF data
+                    for photography purposes, it can be a great way to reduce file size and save memory.
+                </p>
+            </div>
         </div>
     );
 }
