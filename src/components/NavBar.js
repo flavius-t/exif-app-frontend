c25d74a360ff982f37f4d46b8ea62a2575a5fd8c
flavius-t <flavius.t@hotmail.com>
2023-09-22 15:46:17 -0700

    add logout functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import axios from '../api/axios';
+import { toastSuccess } from '../utility/customToasts';
 
 const NavBar = () => {
+    const handleLogout = async (e) => {
+        e.preventDefault();
+        try {
+            const csrf_token = document.cookie.split(';')[0].split('=')[1];
+            const response = await axios.get(
+                '/logout',
+                {
+                    headers: { 'X-CSRF-TOKEN': csrf_token },
+                    withCredentials: true
+                }
+            );
+            if (response.status === 200) {
+                toastSuccess('Logout Successful');
+                console.log(response);
+            }
+        } catch (err) {
+            console.log(err);
+            console.log(err.response);
+        }
+    };
+
     return (
         <nav>
             <h1>EXIF App</h1>
+            {/* TODO: display logout only if auth state is true */}
+            <a onClick={handleLogout}>Logout</a>
         </nav>
     )
 }
